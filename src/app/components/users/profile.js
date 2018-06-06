fc1969079a9b1623dbb9453c2d142f2290135125
Elijah Rwothoromo <rwothoromo@MacBook-Pro-2.local>
2018-06-06 19:59:07 +0300

    [#157838711] Add register user component

---
+++ src/app/components/users/profile.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../../static/css/style.css';
+
+class ProfileUser extends Component {
+  render() {
+    return (
+        <main role="main" className="container-fluid home-bg">
+            <br /><br /><br /><br />
+            <div className="row col-md-12">
+                <h2>User Profile</h2>
+            </div>
+        </main>
+    );
+  }
+}
+
+export default ProfileUser;
