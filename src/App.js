aca316ea6ca0d7a293e1bbdf223fc6483f03ee09
Rwothoromo Elijah <rwothoromoelijah@gmail.com>
2018-06-20 16:36:42 +0300

    feature(register-user): Register user
    This commit allows user registration
    
    [Delivers #158485226]

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
+import { NotificationContainer } from 'react-notifications';
 
 import Index from './app/components/index';
 import RegisterUser from './app/components/users/register';
@@ -9,25 +10,26 @@ import UpdateUser from './app/components/users/update';
 import Layout from './app/components/includes/layout';
 
 const Routes = () => (
-  <Router>
-    <div>
-      <Route exact strict path={"/"} component={Index}/>
-      <Route exact strict path={"/users/register"} component={RegisterUser}/>
-      <Route exact strict path={"/users/login"} component={LoginUser}/>
-      <Route exact strict path={"/users/profile"} component={ProfileUser}/>
-      <Route exact strict path={"/users/update"} component={UpdateUser}/>
-    </div>
-  </Router>
+	<Router>
+		<div>
+			<Route exact strict path={"/"} component={Index}/>
+			<Route exact strict path={"/users/register"} component={RegisterUser}/>
+			<Route exact strict path={"/users/login"} component={LoginUser}/>
+			<Route exact strict path={"/users/profile"} component={ProfileUser}/>
+			<Route exact strict path={"/users/update"} component={UpdateUser}/>
+		</div>
+	</Router>
 )
 
 class App extends Component {
-  render() {
-    return (
-      <Layout>
-        <Routes/>
-      </Layout>
-    );
-  }
+	render() {
+		return (
+			<Layout>
+				<Routes/>
+				<NotificationContainer/>
+			</Layout>
+		);
+	}
 }
 
 export default App;
