d474f9670ff986550de54dbbe85a2e2ddaaf40a8
Elijah Rwothoromo <rwothoromoelijah@gmail.com>
2018-07-02 01:41:35 +0300

    Login test

--- src/tests/auth/RegisterUser.test.js
+++ src/tests/auth/RegisterUser.test.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import { shallow, mount } from 'enzyme';
+import { mount } from 'enzyme';
 import RegisterUser from '../../components/auth/RegisterUser';
 import ReactPasswordStrength from 'react-password-strength';
 import MockAdapter from 'axios-mock-adapter';
+import Axios from 'axios';
+import { apiUrl } from '../../App'
 
 describe('<RegisterUser />', () => {
-	const wrapper = shallow(<RegisterUser />);
-	const mock = new MockAdapter(wrapper.instance().xhr);
+	const wrapper = mount(<RegisterUser />);
+	const mock = new MockAdapter(Axios);
 
 	it('renders without crashing', () => {
 		expect(wrapper.find('.control-label').text()).toContain('Register');
@@ -21,8 +23,31 @@ describe('<RegisterUser />', () => {
 	});
 
 	it('registers user', async () => {
-		mock.onPost('auth/register', {}).reply(201, {
-			message: 'User added'
+		mock.onPost(`${apiUrl}/auth/register`).reply(201, {
+			message: "User added"
 		});
+
+		expect(wrapper.state('registered')).toBe(false);
+
+		// let first_name = wrapper.find('input[name="first_name"]');
+		// first_name.simulate('change', {target: {name: 'first_name', value: 'elijah'}});
+
+		// let last_name = wrapper.find('input[name="last_name"]');
+		// last_name.simulate('change', {target: {name: 'last_name', value: 'rwothoromo'}});
+
+		// let username = wrapper.find('input[name="username"]');
+		// username.simulate('change', {target: {name: 'username', value: 'rwothoromo'}});
+
+		// let password = wrapper.find('password_div').find('input');
+		// password.simulate('change', {target: {name: 'password', value: 'Chang3m31402'}});
+
+		// let confirm_password = wrapper.find('confirm_password_div').find('input');
+		// confirm_password.simulate('change', {target: {name: 'confirm_password', value: 'Chang3m31402'}});
+	
+		// const registerForm = wrapper.find('form');
+		// registerForm.simulate('submit', {
+		// 	preventDefault: () => {}
+		// });
+
 	});
 });
--- src/tests/businesses/RegisterBusiness.test.js
+++ src/tests/businesses/RegisterBusiness.test.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
 import RegisterBusiness from '../../components/businesses/RegisterBusiness';
 import MockAdapter from 'axios-mock-adapter';
 
 describe('<RegisterBusiness />', () => {
-	const wrapper = shallow(<RegisterBusiness />);
+	const wrapper = mount(<RegisterBusiness />);
 	const mock = new MockAdapter(wrapper.instance().xhr);
 
 	it('renders without crashing', () => {
@@ -15,10 +15,29 @@ describe('<RegisterBusiness />', () => {
 		expect(wrapper.find('.other-bg')).toHaveLength(1);
 	});
 
-	it('registers business', () => {
-		mock.onPost('businesses/register').reply(201, {});
-		wrapper.find('[type="submit"]').simulate('submit', {
+	it('registers business', async () => {
+		mock.onPost(`${apiUrl}businesses/register`).reply(201, {
+			message: "Business added"
+		});
+
+		expect(wrapper.state('registered')).toBe(false);
+
+		let name = wrapper.find('input[name="name"]');
+		name.simulate('change', {target: {name: 'name', value: 'jimz auto'}});
+
+		let description = wrapper.find('input[name="description"]');
+		description.simulate('change', {target: {name: 'description', value: 'best auto-part deals' }});
+
+		let category = wrapper.find('input[name="category"]');
+		category.simulate('change', {target: {name: 'category', value: 'cars' }});
+
+		let location = wrapper.find('input[name="location"]');
+		location.simulate('change', {target: {name: 'location', value: 'nakawa' }});
+
+		const registerForm = wrapper.find('form');
+		registerForm.simulate('submit', {
 			preventDefault: () => {}
 		});
-  });
+
+	});
 });
